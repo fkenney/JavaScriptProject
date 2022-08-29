e5f64855581eab412ccd583c1ef8dc98bd30bac5
Felicia Kenney <fkenney@bravocg.com>
2022-08-29 13:42:21 -0400

    added Unsplash API

--- js/script.js
+++ js/script.js
@@ -2,35 +2,37 @@ var form = document.getElementById("destinationForm");
 var wishlist = document.getElementById("container");
 var viewHeading = document.getElementById("viewHeading");
 
+const API_KEY ="YrGKkpykjAhHTJ8YM5BCjxR1iVWsyZs1CIzTMIA1Nug";
+
+
 form.addEventListener("submit", addCard);
 
-function addCard(event){
+// Adds the card to container element
+async function addCard(event){
+    event.preventDefault();
     var destination = event.target[0].value;
     var location = event.target[1].value;
-    var photo = event.target[2].value;
     var description = event.target[3].value;
-    
-    if(!photo){
-        photo = "./images/map.jpg";
-    }
+    var newCard = await buildNewCard(destination, location, description);
 
-    var newCard = buildNewCard(destination, location, photo, description);
     wishlist.appendChild(newCard);
     viewHeading.innerText = "My WishList";
-    event.preventDefault();
     resetForm(event.target);
 }
 
-function buildNewCard( destination, location, photo, description){
-   
+// Creates the card with the information from the user.
+async function buildNewCard( destination, location, description){
+     var col = document.createElement("div");
+     col.classList.add("col");
+
      var card = document.createElement("div");
-     card.classList.add('card');
-     card.style.width = "18rem";
+     card.classList.add("card", "shadow");
+     card.style.width = "17rem";
      card.style.float="right";
      card.style.marginRight = "10px";
 
      var image = document.createElement("img");
-     image.src = photo;
+     image.src = await getImageURL(destination, location);
      image.classList.add("card-img-top");
 
      var cardBody = document.createElement("div");
@@ -65,26 +67,30 @@ function buildNewCard( destination, location, photo, description){
      cardBody.appendChild(deleteButton);
      card.appendChild(image);
      card.appendChild(cardBody); 
-     return card;
+
+     col.appendChild(card);
+     return col;
 
 }
 
+// Resets the form values
 function resetForm(form){
     for( var i = 0; i < form.elements.length; i++){
         form.elements[i].value = "";
     }
 }
 
-function editCard(event){
+// Updates the values on the card
+async function editCard(event){
     var newDestination = prompt("Please enter a new destination name");
     var newLocation = prompt("Please enter a new location");
-    var newUrl = prompt("Please enter a new photo URL");
     var newDesc = prompt("Please enter a new description");
 
     var cardBody = event.target.parentNode;
     var card = cardBody.parentNode;
     if(newDestination){
-      cardBody.childNodes[0].innerText = newDestination;
+       cardBody.childNodes[0].innerText = newDestination;
+
     }
     if(newLocation){
         cardBody.childNodes[1].innerText = newLocation;
@@ -92,13 +98,41 @@ function editCard(event){
     if(newDesc){
        cardBody.childNodes[2].innerText = newDesc;
     }
-    if(newUrl){
-        card.childNodes[0].src = newUrl;
+    if(newDestination || newLocation){
+        var newImage = await getImageURL(newDestination, newLocation);
+        card.childNodes[0].src = newImage;
     }
-
 }
 
+// Deletes the card div object from the container element
 function deleteCard(event){
     var card = event.target.parentNode.parentNode;
     card.remove();
+}
+
+// Gets all values by querying photos by destination and the location
+async function getAllImages(searchVal, searchValTwo){
+    var response =  await fetch('https://api.unsplash.com/search/photos?page=1&query=' +searchVal+','+searchValTwo +'&client_id='+ API_KEY);
+    if(response.status == "200"){
+        return response.json();
+    }else{
+        throw new Error("Invalid search value");
+    }
+}
+
+// Returns a single image from all of the images
+async function getImageURL(destination, location){
+    var image;
+    try{
+        //search image URL
+        var images = await getAllImages(destination, location);
+        image = images.results[0].urls.thumb;
+    }catch(error){
+        //default image URL
+        console.log(error);
+        image = "./images/map.jpg";
+    }
+    finally{
+        return image;
+    }
 }
