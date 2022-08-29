e315af3dac35ffa38186dcb5a89e230e4af37247
Felicia Kenney <fkenney@bravocg.com>
2022-08-29 17:57:23 -0400

    updated variables

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ form.addEventListener("submit", addCard);
 // Adds the card to container element
 async function addCard(event){
     event.preventDefault();
-    var destination = event.target[0].value;
-    var location = event.target[1].value;
-    var description = event.target[3].value;
-    var newCard = await buildNewCard(destination, location, description);
+    let destination = event.target[0].value;
+    let location = event.target[1].value;
+    let description = event.target[3].value;
+    let newCard = await buildNewCard(destination, location, description);
 
     wishlist.appendChild(newCard);
     viewHeading.innerText = "My WishList";
@@ -22,38 +22,38 @@ async function addCard(event){
 
 // Creates the card with the information from the user.
 async function buildNewCard( destination, location, description){
-     var col = document.createElement("div");
+     let col = document.createElement("div");
      col.classList.add("col", "m-2");
 
-     var card = document.createElement("div");
+     let card = document.createElement("div");
      card.classList.add("card", "shadow");
      card.style.width = "17rem";
      card.style.marginRight = "10px";
 
-     var image = document.createElement("img");
+     let image = document.createElement("img");
      image.src = await getImageURL(destination, location);
      image.classList.add("card-img-top");
 
-     var cardBody = document.createElement("div");
+     let cardBody = document.createElement("div");
      cardBody.classList.add("card-body");
 
-     var cardTitle = document.createElement("h5");
+     let cardTitle = document.createElement("h5");
      cardTitle.innerText = destination;
      cardTitle.classList.add("card-title");
     
-     var cardsubTitle = document.createElement("h6");
+     let cardsubTitle = document.createElement("h6");
      cardsubTitle.innerText = location;
 
-     var cardText = document.createElement("p");
+     let cardText = document.createElement("p");
      cardText.innerText = description;
      cardText.classList.add("card-text");
 
-     var editButton = document.createElement("button");
+     let editButton = document.createElement("button");
      editButton.innerHTML = "Edit";
      editButton.classList.add("btn", "btn-warning");
      editButton.addEventListener("click", editCard);
 
-     var deleteButton = document.createElement("button");
+     let deleteButton = document.createElement("button");
      deleteButton.innerHTML = "Delete";
      deleteButton.classList.add("btn", "btn-danger");
      deleteButton.style.float = "right";
@@ -74,19 +74,19 @@ async function buildNewCard( destination, location, description){
 
 // Resets the form values
 function resetForm(form){
-    for( var i = 0; i < form.elements.length; i++){
+    for( let i = 0; i < form.elements.length; i++){
         form.elements[i].value = "";
     }
 }
  
 // Updates the values on the card
 async function editCard(event){
-    var newDestination = prompt("Please enter a new destination name");
-    var newLocation = prompt("Please enter a new location");
-    var newDesc = prompt("Please enter a new description");
+    let newDestination = prompt("Please enter a new destination name");
+    let newLocation = prompt("Please enter a new location");
+    let newDesc = prompt("Please enter a new description");
 
-    var cardBody = event.target.parentNode;
-    var card = cardBody.parentNode;
+    let cardBody = event.target.parentNode;
+    let card = cardBody.parentNode;
     if(newDestination){
        cardBody.childNodes[0].innerText = newDestination;
 
@@ -98,14 +98,14 @@ async function editCard(event){
        cardBody.childNodes[2].innerText = newDesc;
     }
     if(newDestination || newLocation){
-        var newImage = await getImageURL(newDestination, newLocation);
+        let newImage = await getImageURL(newDestination, newLocation);
         card.childNodes[0].src = newImage;
     }
 } 
 
 // Deletes the card div object from the container element
 function deleteCard(event){
-    var card = event.target.parentNode.parentNode;
+    let card = event.target.parentNode.parentNode;
     card.remove();
 }
 
@@ -113,7 +113,7 @@ function deleteCard(event){
 async function getAllImages(searchVal, searchValTwo){
     searchVal = searchVal.replace(/ /g, "");
     searchValTwo = searchValTwo.replace(/ /g, "");
-    var response =  await fetch('https://api.unsplash.com/search/photos?page=1&query=' +searchVal+','+searchValTwo +'&client_id='+ API_KEY);
+    let response =  await fetch('https://api.unsplash.com/search/photos?page=1&query=' +searchVal+','+searchValTwo +'&client_id='+ API_KEY);
     if(response.status == "200"){
         return response.json();
     }else{
@@ -123,10 +123,10 @@ async function getAllImages(searchVal, searchValTwo){
 
 // Returns a single image from all of the images
 async function getImageURL(destination, location){
-    var image;
+    let image;
     try{
         //search image URL
-        var images = await getAllImages(destination, location);
+        let images = await getAllImages(destination, location);
         image = images.results[0].urls.thumb;
     }catch(error){
         //default image URL
@@ -137,3 +137,4 @@ async function getImageURL(destination, location){
         return image;
     }
 } 
+
