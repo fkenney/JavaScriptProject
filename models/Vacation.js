0ea7618a2c85be96c551971a809416c89922ec40
Felicia Kenney <fkenney@bravocg.com>
2022-09-06 10:40:03 -0400

    uploaded Models

---
+++ models/Vacation.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+
+const Schema = mongoose.Schema;
+
+const vacationSchema = new Schema({
+    destination : String,
+    location: String,
+    description : String,
+    image: String,
+})
+
+
+export default mongoose.model('Vacation', vacationSchema)
